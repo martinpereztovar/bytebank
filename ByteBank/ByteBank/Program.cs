cc17491736e4e4a0707a85350d0d04c3ffbee850
Martin Perez Tovar <martinpereztovar@gmail.com>
2023-01-03 10:38:29 -0300

    Adicionando varias funções ao banco

--- ByteBank/ByteBank/Program.cs
+++ ByteBank/ByteBank/Program.cs
@@ -2,6 +2,7 @@
 {
     public class Program
     {
+        // Mostrando o menu principal
         static void ShowMenu()
         {
             Console.WriteLine("Bem-vindo!");
@@ -15,15 +16,75 @@
             Console.WriteLine("0 - Sair do programa");
         }
 
-        static void RegisterNewUser(List<string> cpfs, List<string> names, List<string> senhas, List<double> saldos)
+        // Inicio dos métodos ou funções dentro do programa
+
+        // --- Função para registrar novos usuários---
+        static void RegisterNewUser(List<string> cpfs, List<string> names, List<string> passwords, List<double> balances)
         {
             Console.WriteLine("Digite o CPF: ");
             cpfs.Add(Console.ReadLine());
             Console.WriteLine("Digite o nome: ");
             names.Add(Console.ReadLine());
             Console.WriteLine("Digite a senha: ");
-            senhas.Add(Console.ReadLine());
-            saldos.Add(0);
+            passwords.Add(Console.ReadLine());
+            balances.Add(0);
+        }
+
+        // --- Função para deletar novos usuários---
+        static void DeleteUser(List<string> cpfs, List<string> names, List<string> passwords, List<double> balances)
+        {
+            Console.WriteLine("Digite o CPF: ");
+            string cpfToDelete = Console.ReadLine();
+            int indexToDelete = cpfs.FindIndex(cpf => cpf == cpfToDelete);
+
+            if (indexToDelete == -1)
+            {
+                Console.WriteLine("Conta não encontrada.");
+            }
+
+            cpfs.Remove(cpfToDelete);
+            names.RemoveAt(indexToDelete);
+            passwords.RemoveAt(indexToDelete);
+            balances.RemoveAt(indexToDelete);
+
+            Console.WriteLine("Conta deletada corretamente.");
+
+        }
+
+        // --- Função para mostrar a conta do usuário---
+        static void ShowUserAccount(int index, List<string> cpfs, List<string> names, List<double> balances)
+        {
+            Console.WriteLine($"CPF: {cpfs[index]} || Nome: {names[index]} || Saldo: R${balances[index]:F2}");
+        }
+
+        // --- Função para mostrar todos os usuários---
+        static void ListUsers(List<string> cpfs, List<string> names, List<double> balances)
+        {
+            for (int i = 0; i < cpfs.Count; i++)
+            {
+                ShowUserAccount(i, cpfs, names, balances);
+            }
+        }
+
+        // --- Função para mostrar um usuário---
+        static void ShowUser(List<string> cpfs, List<string> names, List<double> balances)
+        {
+            Console.WriteLine("Digite o CPF: ");
+            string cpfToShow = Console.ReadLine();
+            int indexToShow = cpfs.FindIndex(cpf => cpf == cpfToShow);
+
+            if (indexToShow == -1)
+            {
+                Console.WriteLine("Usuário ou conta não encontrados.");
+            }
+
+            ShowUserAccount(indexToShow, cpfs, names, balances);
+        }
+
+        // --- Função para mostrar saldos---
+        static void ShowUserBalance(List<string> cpfs, List<string> names, List<double> balances)
+        {
+
         }
         public static void Main(string[] args)
         {
