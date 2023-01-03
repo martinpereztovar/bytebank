53b1cbf2c90e7a0ede6f219e31d584d1f62f27d3
Martin Perez Tovar <martinpereztovar@gmail.com>
2023-01-03 09:18:31 -0300

    Mostrar menu

--- ByteBank/ByteBank/Program.cs
+++ ByteBank/ByteBank/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ByteBank
+{
+    public class Program
+    {
+        static void ShowMenu()
+        {
+            Console.WriteLine("Bem-vindo!");
+            Console.Write("Digite a opção desejada: ");
+            Console.WriteLine("1 - Inserir um novo usuário");
+            Console.WriteLine("2 - Deletar um usuário");
+            Console.WriteLine("3 - Listar todas as contas registradas");
+            Console.WriteLine("4 - Ver detalhes de um usuário");
+            Console.WriteLine("5 - Ver saldo no banco");
+            Console.WriteLine("6 - Manipular a conta");
+            Console.WriteLine("0 - Sair do programa");
+        }
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
